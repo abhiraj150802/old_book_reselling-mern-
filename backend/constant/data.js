d2c62348034a3685959dfe9710368ced684778cb
abhiraj raj <abhiraj150802@gmail.com>
2024-03-12 22:18:46 +0530

    further addision

--- backend/constant/data.js
+++ backend/constant/data.js
@@ -1,12 +1,142 @@
 
+// export const products = [
+//     { 
+//         id: 'product1',
+//         url: 'https://rukminim2.flixcart.com/image/300/300/kpinwy80/book/y/7/w/cracking-the-coding-interview-189-programming-questions-and-original-imag3qh8hpmzuwza.jpeg?q=70', 
+//         detailUrl: 'https://rukminim2.flixcart.com/image/832/832/kpinwy80/book/y/7/w/cracking-the-coding-interview-189-programming-questions-and-original-imag3qh8hpmzuwza.jpeg?q=70&crop=false',
+//         title: {
+//             shortTitle: 'Career',
+//             longTitle: 'Cracking the Coding Interview  (English, Paperback, McDowell Gayle Laakmann)'
+//         }, 
+//         price: {
+//             mrp: 1195,
+//             cost: 625,
+//             discount: '47%'
+//         },
+//         quantity: 1,
+//         description: 'This is one of the best choice for interview preparation and it is a way to IT Companies',
+//         discount: 'Extra 10% Off', 
+//         tagline: 'Deal of the day' 
+//     },
+//     { 
+//         id: 'product2',
+//         url: 'https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70', 
+//         detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70',
+//         title: {
+//             shortTitle: 'Sandwich Makers',
+//             longTitle: 'Flipkart SmartBuy Sandwich 01 Grill  (Black)'
+//         },
+//         price: {
+//             mrp: 1499,
+//             cost: 899,
+//             discount: '40%'
+//         },
+//         quantity: 1,
+//         description: 'This non-stick sandwich toaster .easy to use and very handy. Directly hold over flame to make tasty toasts and toasted sandwiches. Specially designed by keeping your needs in mind, the sandwich maker makes whatever youre doing simpler, smarter and better',
+//         discount: 'From 99+5% Off', 
+//         tagline: 'Pestige, Nova & more' 
+//     },
+//     { 
+//         id: 'product3',
+//         url: 'https://rukminim1.flixcart.com/image/150/150/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70', 
+//         detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70', 
+//         title: {
+//             shortTitle: 'Fitness Gear',
+//             longTitle: 'AJRO DEAL New Adjustable Single Resistance Tube (Multicolor) Resistance Tube  (Multicolor)'
+//         }, 
+//         price: {
+//             mrp: 499,
+//             cost: 166,
+//             discount: '66%'
+//         },
+//         quantity: 1,
+//         description: 'This unique product can tone your back muscles, reduce belly fat, improve blood circulation and also improves your body posture. It increases the stamina, energy and vitality of the body. The elastic resistance of the rubber training rope can be used to train and exercise in whichever way you want, according to your physical needs.',
+//         discount: 'Upto 70% Off', 
+//         tagline: 'Deal of the Day' 
+//     },
+//     { 
+//         id: 'product4',
+//         url: 'https://rukminim1.flixcart.com/image/300/300/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70', 
+//         detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70',
+//         title: {
+//             shortTitle: 'Smart Watches',
+//             longTitle: 'Molife Sense 500 Smartwatch  (Black Strap, Freesize)',
+//         }, 
+//         price: {
+//             mrp: 6999,
+//             cost: 4049,
+//             discount: '42%'
+//         },
+//         quantity: 1,
+//         description: 'The Molife Sense 500, a brilliant smartwatch with a beautiful large display. Say hello to the infinity 1.7-inch display with 2.5D curved edges. Thanks to seamless Bluetooth 5.0 connectivity, you wont have to keep waiting. Bring a change to your outfit every day with changeable straps. A splash of color every day keeps the boredom away.',
+//         discount: 'Grab Now', 
+//         tagline: 'Best Seller' 
+//     },
+//     { 
+//         id: 'product5',
+//         url: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70',
+//         detailUrl: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70', 
+//         title: {
+//             shortTitle: 'Trimmers, Dryers & more',
+//             longTitle: 'Nova Professional NHP 8220 Hair Dryer  (1800 W, Multicolor)'
+//         }, 
+//         price: {
+//             mrp: 1899,
+//             cost: 1124,
+//             discount: '40%'
+//         },
+//         quantity: 1,
+//         description: '',
+//         discount: 'From ₹499', 
+//         tagline: 'Kubra, Nova & more' 
+//     },
+//     { 
+//         id: 'product6',
+//         url: 'https://rukminim1.flixcart.com/image/150/150/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70',
+//         detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70', 
+//         title: {
+//             shortTitle: 'Table Fans',
+//             longTitle: 'Portable 300 mm Ultra High Speed 3 Blade Table Fan  (Black, Pack of 1)'
+//         }, 
+//         price: {
+//             mrp: 2250,
+//             cost: 1199,
+//             discount: '46%'
+//         },
+//         quantity: 1,
+//         description: 'Table Fan. Perfect size fan for use on a table, desk or in an RV. Whisper quiet, powerful airflow and reliable operation in a compact 6" size. Two adjustable speeds to customize airflow: high or low settings. Tough break-resistant ABS plastic blades. ',
+//         discount: 'Minimum 40% Off', 
+//         tagline: 'Top Selling' 
+//     },
+//     { 
+//         id: 'product7',
+//         url: 'https://rukminim1.flixcart.com/image/150/150/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70',
+//         detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70', 
+//         title: {
+//             shortTitle: 'Headphones',
+//             longTitle: 'boAt Rockerz 235v2 with ASAP charging Version 5.0 Bluetooth Headset '
+//         }, 
+//         price: {
+//             mrp: 2990,
+//             cost: 1199,
+//             discount: '59%'
+//         },
+//         quantity: 1,
+//         description: 'Let music brighten up your mood anytime, anywhere with the boAt 235v2 Fast Charging Bluetooth Headset. This Bluetooth headset features a Call Vibration Alert, a Fast Charging Technology, and Easy Access Controls to listen to and manage your favorite music with ease.',
+//         discount: 'Minimum 50% Off', 
+//         tagline: 'Grab Now!' 
+//     }
+// ];
+
+
 export const products = [
     { 
         id: 'product1',
-        url: 'https://rukminim1.flixcart.com/image/150/150/kapoo7k0/electric-kettle/p/6/s/pigeon-favourite-original-imafs7xhj5uwgrh4.jpeg?q=70', 
-        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kapoo7k0/electric-kettle/p/6/s/pigeon-favourite-original-imafs7xhj5uwgrh4.jpeg?q=70',
+        url: 'https://rukminim2.flixcart.com/image/300/300/kpinwy80/book/y/7/w/cracking-the-coding-interview-189-programming-questions-and-original-imag3qh8hpmzuwza.jpeg?q=70', 
+        detailUrl: 'https://rukminim2.flixcart.com/image/832/832/kpinwy80/book/y/7/w/cracking-the-coding-interview-189-programming-questions-and-original-imag3qh8hpmzuwza.jpeg?q=70&crop=false',
         title: {
-            shortTitle: 'Home & Kitchen',
-            longTitle: 'Pigeon FAVOURITE Electric Kettle  (1.5 L, Silver, Black)'
+            shortTitle: 'Career',
+            longTitle: 'Cracking the Coding Interview  (English, Paperback, McDowell Gayle Laakmann)'
         }, 
         price: {
             mrp: 1195,
@@ -14,17 +144,17 @@ export const products = [
             discount: '47%'
         },
         quantity: 1,
-        description: 'This electric kettle from Pigeon will soon become a travelers best friend, a hostelite saviour and an answer to all the midnight cravings. With this handy appliance, you can boil water and use it to make instant noodles, packet soup, coffee and green tea.',
+        description: 'This is one of the best choice for interview preparation and it is a way to IT Companies',
         discount: 'Extra 10% Off', 
         tagline: 'Deal of the day' 
     },
     { 
         id: 'product2',
-        url: 'https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70', 
-        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kl6wx3k0/sandwich-maker/8/r/d/sandwich-01-flipkart-smartbuy-original-imagydds4zthxt8z.jpeg?q=70',
+        url: 'https://rukminim2.flixcart.com/image/300/300/kjd6nww0-0/book/x/c/o/marketing-management-15e-with-free-indian-cases-book-original-imafyxhqvhyps8fy.jpeg?q=70',
+        detailUrl: 'https://rukminim2.flixcart.com/image/832/832/kjd6nww0-0/book/x/c/o/marketing-management-15e-with-free-indian-cases-book-original-imafyxhqvhyps8fy.jpeg?q=70&crop=false',
         title: {
-            shortTitle: 'Sandwich Makers',
-            longTitle: 'Flipkart SmartBuy Sandwich 01 Grill  (Black)'
+            shortTitle: 'Popular Books',
+            longTitle: 'Marketing Management 15e (with INDIAN CASES book)  (English, Paperback, Kotler Philip)'
         },
         price: {
             mrp: 1499,
@@ -32,17 +162,17 @@ export const products = [
             discount: '40%'
         },
         quantity: 1,
-        description: 'This non-stick sandwich toaster .easy to use and very handy. Directly hold over flame to make tasty toasts and toasted sandwiches. Specially designed by keeping your needs in mind, the sandwich maker makes whatever youre doing simpler, smarter and better',
+        description: 'This is one of the best choice for interview preparation and it is a way to IT Companies',
         discount: 'From 99+5% Off', 
-        tagline: 'Pestige, Nova & more' 
+        tagline: 'Marketing and Management' 
     },
     { 
         id: 'product3',
-        url: 'https://rukminim1.flixcart.com/image/150/150/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70', 
-        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70', 
+        url: 'https://rukminim2.flixcart.com/image/300/300/jyg5lzk0/book/4/5/3/problems-and-solutions-in-physical-chemistry-for-jee-main-and-original-imafgzp8f3efxzvt.jpeg?q=70', 
+        detailUrl: 'https://rukminim2.flixcart.com/image/832/832/jyg5lzk0/book/4/5/3/problems-and-solutions-in-physical-chemistry-for-jee-main-and-original-imafgzp8f3efxzvt.jpeg?q=70&crop=false', 
         title: {
-            shortTitle: 'Fitness Gear',
-            longTitle: 'AJRO DEAL New Adjustable Single Resistance Tube (Multicolor) Resistance Tube  (Multicolor)'
+            shortTitle: 'Chemistry',
+            longTitle: 'Problems in Physical Chemistry for Jee Main and Advanced  (English, Paperback, Kumar Neeraj)'
         }, 
         price: {
             mrp: 499,
@@ -56,11 +186,11 @@ export const products = [
     },
     { 
         id: 'product4',
-        url: 'https://rukminim1.flixcart.com/image/300/300/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70', 
-        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70',
+        url: 'https://rukminim2.flixcart.com/image/300/300/kfwvcsw0/book/0/1/3/26-years-upsc-ias-ips-prelims-topic-wise-solved-papers-1-2-1995-original-imafw9zyjhsc9ccb.jpeg?q=70', 
+        detailUrl: 'https://rukminim2.flixcart.com/image/832/832/jwzabgw0/book/3/8/0/25-years-upsc-ias-ips-prelims-topic-wise-solved-papers-1-2-1995-original-imafhjd85x3hcyrc.jpeg?q=70&crop=false',
         title: {
-            shortTitle: 'Smart Watches',
-            longTitle: 'Molife Sense 500 Smartwatch  (Black Strap, Freesize)',
+            shortTitle: 'Popular Books',
+            longTitle: '25 years of prelims questions',
         }, 
         price: {
             mrp: 6999,
@@ -74,11 +204,11 @@ export const products = [
     },
     { 
         id: 'product5',
-        url: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70',
-        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/k3uhhu80/hair-dryer/n/m/t/nova-2800-w-professional-nhp-8220-original-imafmvwfhmzsxdrw.jpeg?q=70', 
+        url: 'https://rukminim2.flixcart.com/image/300/300/k9re3rk0/book/2/5/3/xamidea-social-science-class-9-2020-21-original-imafrhh9kxppzbex.jpeg?q=70',
+        detailUrl: 'https://rukminim2.flixcart.com/image/832/832/k9re3rk0/book/2/5/3/xamidea-social-science-class-9-2020-21-original-imafrhh9kxppzbex.jpeg?q=70&crop=false', 
         title: {
-            shortTitle: 'Trimmers, Dryers & more',
-            longTitle: 'Nova Professional NHP 8220 Hair Dryer  (1800 W, Multicolor)'
+            shortTitle: 'Exam idea',
+            longTitle: 'Xam Idea Social Science Class 9 Cbse Examination 2020-2021 First Edition  (English, Paperback, unknown)'
         }, 
         price: {
             mrp: 1899,
@@ -86,17 +216,18 @@ export const products = [
             discount: '40%'
         },
         quantity: 1,
-        description: '',
+        description: 'To ace the board exam , this books are like path to success',
         discount: 'From ₹499', 
-        tagline: 'Kubra, Nova & more' 
+        tagline: 'Top Academic Picks' 
     },
     { 
         id: 'product6',
-        url: 'https://rukminim1.flixcart.com/image/150/150/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70',
-        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kk01pjk0/fan/d/d/l/tiktik-quiet-portable-table-fan-zigma-original-imafzg7ftzuckpad.jpeg?q=70', 
+        url: 'https://rukminim2.flixcart.com/image/300/300/xif0q/regionalbooks/e/y/f/brahmastra-complete-maths-multicolored-formula-book-second-original-imaguffwjktawwe7.jpeg?q=90',
+        detailUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/regionalbooks/e/y/f/brahmastra-complete-maths-multicolored-formula-book-second-original-imaguffwjktawwe7.jpeg?q=70&crop=false',
+
         title: {
-            shortTitle: 'Table Fans',
-            longTitle: 'Portable 300 mm Ultra High Speed 3 Blade Table Fan  (Black, Pack of 1)'
+            shortTitle: 'Competition Crackers',
+            longTitle: 'BRAHMASTRA Complete Maths Multicolored Formula Book Second Edition BILINGUAL (Paperback, Aditya Ranjan Sir)'
         }, 
         price: {
             mrp: 2250,
@@ -110,11 +241,11 @@ export const products = [
     },
     { 
         id: 'product7',
-        url: 'https://rukminim1.flixcart.com/image/150/150/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70',
-        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70', 
+        url: 'https://rukminim2.flixcart.com/image/300/300/xif0q/regionalbooks/t/l/2/neetu-singh-volume-1-english-for-all-government-exams-original-imagwzjgnsfwzrb9.jpeg?q=90',
+        detailUrl: 'https://rukminim2.flixcart.com/image/832/832/xif0q/regionalbooks/t/l/2/neetu-singh-volume-1-english-for-all-government-exams-original-imagwzjgnsfwzrb9.jpeg?q=70&crop=false', 
         title: {
-            shortTitle: 'Headphones',
-            longTitle: 'boAt Rockerz 235v2 with ASAP charging Version 5.0 Bluetooth Headset '
+            shortTitle: 'English Communication',
+            longTitle: 'Neetu Singh Volume 1 | English | For All Government Exams  (Paperback, Neetu Singh)'
         }, 
         price: {
             mrp: 2990,
